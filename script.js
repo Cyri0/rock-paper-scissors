43caff07f115628b3caa591c8b2d94169e696f0d
Cyri0 <nemestamas94@gmail.com>
2023-03-14 11:05:16 +0100

    final

--- script.js
+++ script.js
@@ -8,11 +8,13 @@ options.push({'name' : 'Rock', 'emoji': '&#9994;'})
 options.push({'name' : 'Paper', 'emoji': '&#9995;'})
 options.push({'name' : 'Scissors', 'emoji': '&#9996;'})
 
-
+let playerHandEmoji = null
 
 for(let i = 0; i< choiceButtons.length; i++){
     choiceButtons[i].onclick = ()=>{
-      alert(" " + choiceButtons[i].id)
+      reset()
+      playerHandEmoji = choiceButtons[i].innerHTML
+      go()
     }
     choiceButtons[i].innerHTML = options[i].emoji
 }
@@ -20,20 +22,28 @@ for(let i = 0; i< choiceButtons.length; i++){
 function reset(){
   playerChoice.innerHTML = "&#9994;"
   enemyChoice.innerHTML = "&#9994;"
-
 }
 
 reset()
 
 function go (){
   let mod = -1
-
-  setInterval(() => {
+  let count = 0
+  let goInterval = setInterval(() => {
     enemyChoice.style.transform = 'rotate('+10*mod+'deg)'
-
     playerChoice.style.transform = 'rotateY(180deg) rotate('+(-90 + 10*mod)+'deg)'
     mod = mod* -1
+
+    count += 1
+    if(count == 6){
+      clearInterval(goInterval)
+      showHands()
+    }
   }, 200);
 }
 
-go()
+function showHands(){
+  let enemyChoiceRnd = Math.floor(Math.random()*3)
+  enemyChoice.innerHTML = options[enemyChoiceRnd].emoji
+  playerChoice.innerHTML = playerHandEmoji
+}
